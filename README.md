445ba656f1d75cfd3ccff97d1ab929954d5e146f
louis <louis@systemli.org>
2019-09-04 20:32:52 +0200

    initial commit
    Signed-off-by: louis <louis@systemli.org>

---
+++ README.md
@@ -0,0 +1,3 @@
+# ep_stats_extended
+
+Plugin for Etherpad which expose a new stats endpoint `/stats_extended`
---
+++ index.js
@@ -0,0 +1,31 @@
+var API = require('ep_etherpad-lite/node/db/API');
+var stats = require('ep_etherpad-lite/node/stats');
+var padsCount = stats.counter('pads', {count: padCount().length});
+
+exports.registerRoute = function (hook_name, args, cb) {
+    args.app.get('/stats_extended', function (req, res) {
+        res.json(stats.toJSON());
+    });
+};
+
+exports.padLoad = function (hook_name, context, cb) {
+    var pad = context.pad;
+    if (pad.head === 0) {
+        console.log('stats inc');
+        padsCount.inc(1);
+    }
+};
+
+exports.padRemove = function (hook_name, context, cb) {
+    padsCount.dec(1);
+};
+
+function padCount() {
+    var pads = [];
+
+    API.listAllPads(function (err, data) {
+        pads = data.padIDs;
+    });
+
+    return pads;
+}
