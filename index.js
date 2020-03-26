c0dcf3dfa22624290f41b2d5b23d4b0057dc55f8
louis <louis@systemli.org>
2020-03-26 20:39:15 +0100

    check for string

--- index.js
+++ index.js
@@ -14,13 +14,13 @@ stats.gauge('totalPads', function () {
 stats.gauge('pads', function () {
   return {
     'default': pads.filter(function (padID) {
-      return !padID.endsWith('-keep') && !padID.endsWith('-temp')
+      return typeof padID !== 'string' ? false : !padID.endsWith('-keep') && !padID.endsWith('-temp')
     }).length,
     'temp': pads.filter(function (padID) {
-      return padID.endsWith('-temp')
+      return typeof padID !== 'string' ? false : padID.endsWith('-temp')
     }).length,
     'keep': pads.filter(function (padID) {
-      return padID.endsWith('-keep')
+      return typeof padID !== 'string' ? false : padID.endsWith('-keep')
     }).length
   }
 })
